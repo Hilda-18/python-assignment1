6dbfdfb3b15cac5ec1bc41ac5696dbd1ff4ca781
Hilda_muritu <hildamuritu@gmail.com>
2025-08-01 20:31:03 +0300

    add calculation.py

---
+++ calculations.py
@@ -0,0 +1,12 @@
+# Multiplacation program in Python
+
+# Ask the user for two numbers and multiply them
+num1 = 25
+
+num2 = 15
+
+# Multiply the two numbers
+result = num1 * num2
+
+# Print the result
+print("result:", result)
